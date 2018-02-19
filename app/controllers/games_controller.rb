ab30c1599cbc8f61fef5123177e1419d1e1917e9
Matt Roach <mattroach@mac.com>
2018-02-19 18:31:06 +0100

    ex2

---
+++ app/controllers/games_controller.rb
@@ -0,0 +1,36 @@
+require 'open-uri'
+
+class GamesController < ApplicationController
+  def new
+    @letters = (0..10).map { ('a'..'z').to_a[rand(26)] }
+  end
+
+  def score
+    @attempt_url = params[:attempt]
+    @attempt = @attempt_url.split("")
+    if @attempt == []
+      @result = "No result"
+    elsif grid_check(@attempt, Aletters) == false
+      @result = "Sorry but #{@attempt} cannot be built out of #{@letters}"
+# <!-- api call is false -->
+    elsif api_check(@attempt) == false
+      @result = "Sorry but #{@attempt} is not a valid English word..."
+# The word is valid according to the grid and is an English word
+    else
+      @result = "Congratulations! #{@attempt} seems to be an English word!"
+    end
+  end
+
+  def api_check(attempt)
+    url = 'https://wagon-dictionary.herokuapp.com/#{attempt}'
+      response_file = open(url).read
+      response = JSON.parse(response)
+      response.found
+  end
+
+  def grid_check(attempt, letters)
+    attempt.all? { |i| letters.include?(i) }
+  end
+end
+
+
