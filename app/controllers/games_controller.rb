9688f6d2b571ac007a1dbd4328b09541ba734cf0
Matt Roach <mattroach@mac.com>
2018-02-20 11:27:44 +0100

    ex2

--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -7,13 +7,14 @@ class GamesController < ApplicationController
 
   def score
     @attempt_url = params[:attempt]
+    @letters = params[:letters]
     @attempt = @attempt_url.split("")
     if @attempt == []
       @result = "No result"
-    elsif grid_check(@attempt, Aletters) == false
+    elsif grid_check(@attempt, @letters) == false
       @result = "Sorry but #{@attempt} cannot be built out of #{@letters}"
 # <!-- api call is false -->
-    elsif api_check(@attempt) == false
+    elsif api_check(@attempt_url) == false
       @result = "Sorry but #{@attempt} is not a valid English word..."
 # The word is valid according to the grid and is an English word
     else
@@ -22,10 +23,10 @@ class GamesController < ApplicationController
   end
 
   def api_check(attempt)
-    url = 'https://wagon-dictionary.herokuapp.com/#{attempt}'
+    url = "https://wagon-dictionary.herokuapp.com/#{attempt}"
       response_file = open(url).read
-      response = JSON.parse(response)
-      response.found
+      response = JSON.parse(response_file)
+      response[ "found" ]
   end
 
   def grid_check(attempt, letters)
